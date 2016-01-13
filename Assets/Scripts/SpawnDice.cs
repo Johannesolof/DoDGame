67d1d285eaf431d520b018176399a381ee3f77ee
Ekengren <johannes.o.andersson@gmail.com>
2016-01-13 19:08:16 +0100

    DiceTest

---
+++ Assets/Scripts/SpawnDice.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnDice : MonoBehaviour
+{
+
+    public GameObject dice;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Casts the ray and get the first game object hit
+            Physics.Raycast(ray, out hit);
+            Debug.Log("This hit at " + hit.point);
+	        var pos = hit.point;
+	        pos.y += 2;
+            if (dice != null)
+                Instantiate(dice, pos, Quaternion.identity);
+	    }
+	}
+}
