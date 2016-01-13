875332ef13e8f4ced9a0bf467756ed26582d1bf2
JudeJohan <jae91m@gmail.com>
2016-01-13 17:52:06 +0100

    Nätverkstestinggrejer

--- Assets/Scripts/NetworkingTest.cs
+++ Assets/Scripts/NetworkingTest.cs
@@ -1,15 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 public class NetworkingTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public bool isAtStartup = true;
+
+	NetworkClient myClient;
+
+	void Update () 
+	{
+		if (isAtStartup)
+		{
+			if (Input.GetKeyDown(KeyCode.S))
+			{
+				SetupServer();
+			}
+
+			if (Input.GetKeyDown(KeyCode.C))
+			{
+				SetupClient();
+			}
+
+			if (Input.GetKeyDown(KeyCode.B))
+			{
+				SetupServer();
+				SetupLocalClient();
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnGUI()
+	{
+		if (isAtStartup)
+		{
+			GUI.Label(new Rect(2, 10, 150, 100), "Press S for server");     
+			GUI.Label(new Rect(2, 30, 150, 100), "Press B for both");       
+			GUI.Label(new Rect(2, 50, 150, 100), "Press C for client");
+		}
+	}
+
+	// Create a server and listen on a port
+	public void SetupServer()
+	{
+		NetworkServer.Listen(47624);
+		isAtStartup = false;
+	}
+
+	// Create a client and connect to the server port
+	public void SetupClient()
+	{
+		myClient = new NetworkClient();
+		myClient.RegisterHandler(MsgType.Connect, OnConnected);
+		myClient.Connect("213.114.70.247", 47624);
+		isAtStartup = false;
+	}
+
+	// Create a local client and connect to the local server
+	public void SetupLocalClient()
+	{
+		myClient = ClientScene.ConnectLocalServer();
+		myClient.RegisterHandler(MsgType.Connect, OnConnected);
+		isAtStartup = false;
+	}
+
+	// client function
+	public void OnConnected(NetworkMessage netMsg)
+	{
+		Debug.Log("Connected to server " + netMsg.conn.address);
 	}
 }
+
