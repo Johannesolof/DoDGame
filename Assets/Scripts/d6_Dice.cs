e39a2c5eb07a38ff4d4edaefd0a376c90e2323fd
Ekengren <johannes.o.andersson@gmail.com>
2016-01-17 22:51:26 +0100

    Change colliders and render meshes

--- Assets/Scripts/d6_Dice.cs
+++ Assets/Scripts/d6_Dice.cs
@@ -16,3 +16,23 @@ public class d6_Dice : BaseDice {
         };
     }
 }
+
+public class d8_Dice : BaseDice
+{
+
+    protected override List<Normal> GetNormals(int sides)
+    {
+        return new List<Normal>
+        {
+            new Normal {Value = 1, Direction = new Vector3(0, 1, 0)},
+            new Normal {Value = 2, Direction = new Vector3(-0.8f, 0.6f, -1)},
+            new Normal {Value = 3, Direction = new Vector3(1, 0, 0)},
+            new Normal {Value = 4, Direction = new Vector3(-1, 0, 0)},
+            new Normal {Value = 5, Direction = new Vector3(0, 0, 1)},
+            new Normal {Value = 6, Direction = new Vector3(0, -1, 0)},
+
+            new Normal {Value = 7, Direction = new Vector3(0.8f, -0.6f, -1)},
+            new Normal {Value = 8, Direction = new Vector3(-0.8f, -0.6f, -1)},
+        };
+    }
+}
