660badf5ea93e5b98e5732ad249500ababd9aa33
JudeJohan <jae91m@gmail.com>
2016-01-17 22:57:00 +0100

    d6

--- Assets/Scripts/d6_Dice.cs
+++ Assets/Scripts/d6_Dice.cs
@@ -1,6 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+public class d4_Dice : BaseDice {
+
+	protected override List<Normal> GetNormals(int sides)
+	{
+		return new List<Normal>
+		{
+			new Normal {Value = 1, Direction = new Vector3(-0.5f, -0.3f, 0.8f)},
+			new Normal {Value = 2, Direction = new Vector3(-0.5f, -0.3f, -0.8f)},
+			new Normal {Value = 3, Direction = new Vector3(0.9f, -0.3f, 0f)},
+			new Normal {Value = 4, Direction = new Vector3(0f, 1f, 0f)},
+		};
+	}
+}
+
 public class d6_Dice : BaseDice {
 
     protected override List<Normal> GetNormals(int sides)
