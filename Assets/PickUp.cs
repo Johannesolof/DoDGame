7866f569179eee2e9b7b4758cb1e1fe7bb6b4f92
Ekengren <johannes.o.andersson@gmail.com>
2016-01-18 21:09:27 +0100

    Zoom test start

---
+++ Assets/PickUp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+
+    private Transform _startTransform;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _startTransform = transform;
+        
+	}
+    
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnMouseDown()
+    {
+        Camera camera = Camera.main;
+        
+        var ray = camera.ScreenPointToRay(new Vector3(camera.pixelWidth / 2f, camera.pixelHeight / 2f));
+        var pos = ray.GetPoint(3);
+
+        var renderer = GetComponent<Renderer>();
+        
+
+        
+
+
+        transform.rotation = Quaternion.LookRotation(ray.direction);
+
+        transform.position = pos;
+    }
+}
